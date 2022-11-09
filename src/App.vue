3d76bb7d733d33cb92740607a9b43aea13fe3402
Mike Greber <mikegreberdevelopment@gmail.com>
2022-11-09 11:40:33 -0700

    add navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
+  <MainNav />
   <h1>Hello Vue</h1>
 </template>
 
 <script>
+import MainNav from "@/components/MainNav";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>

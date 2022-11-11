d949b721641e127cdbef6d1b7ffeda5d3b39d79c
Mike Greber <mikegreberdevelopment@gmail.com>
2022-11-10 19:28:42 -0700

    add text-input component

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,12 +6,7 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="role" placeholder="Software engineer" />
       </div>
       <span
         class="flex items-center h-full px-3 border-l border-r border-brand-gray-3 bg-brand-gray-2"
@@ -19,12 +14,7 @@
       >
       <div class="relative flex items-center flex-1 h-full ml-3">
         <label class="absolute left-0 -top-10">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Edmonton, AB"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input v-model="location" placeholder="Edmonton, AB" />
         <action-button text="Search" type="secondary" class="rounded-r-3xl" />
       </div>
     </div>
@@ -34,9 +24,10 @@
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import ActionButton from "@/components/Shared/ActionButton";
+import TextInput from "../Shared/TextInput";
 export default {
   name: "JobSearchForm",
-  components: { ActionButton, FontAwesomeIcon },
+  components: { TextInput, ActionButton, FontAwesomeIcon },
   data() {
     return {
       role: "",
---
+++ src/components/Shared/TextInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <input
+    :value="modelValue"
+    :placeholder="placeholder"
+    type="text"
+    class="w-full text-lg font-normal focus:outline-none"
+    @input="handleInput"
+  />
+</template>
+
+<script>
+export default {
+  name: "TextInput",
+  props: {
+    placeholder: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    handleInput($event) {
+      this.$emit("update:modelValue", $event.target.value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
